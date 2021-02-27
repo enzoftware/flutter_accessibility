cba537886155c0fb4c977bd0e825596a0583401b
Enzo Lizama <lizama.enzo@gmail.com>
2021-02-27 16:30:59 -0500

    update data test

--- lib/data/expense_data.dart
+++ lib/data/expense_data.dart
@@ -2,28 +2,21 @@ import 'package:flutter_expense_tracker/domain/expense.dart';
 
 final testExpensesData = [
   Expense(
-    amount: 1292.1,
+    amount: 100.0,
     description: 'University',
     date: '12/12/2020',
     type: ExpenseType.increase,
   ),
   Expense(
-    amount: 122.1,
+    amount: 200.0,
     description: 'Gym',
     date: '01/01/2021',
     type: ExpenseType.whitdrawal,
   ),
   Expense(
-    amount: 10.5,
+    amount: 300.0,
     description: 'Food',
     date: '01/02/2021',
     type: ExpenseType.increase,
   ),
 ];
-
-final expenseTest = Expense(
-  amount: 100.0,
-  description: 'Cat food',
-  date: '04/02/2021',
-  type: ExpenseType.increase,
-);
